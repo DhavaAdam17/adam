7d0edc330455c1cb4db26d927fc9ceb144a31b0a
DhavaAdam17 <145167805+DhavaAdam17@users.noreply.github.com>
2024-05-01 13:54:21 +0700

    Add files via upload

---
+++ responsive.js
@@ -0,0 +1,17 @@
+const iconnav = document.getElementById(icon-nav);
+
+iconnav.addEventListener("onclick", () => {alert("klik disini")});
+
+alert("halo guys");
+
+document("haloo broo");
+
+this.Document("mari ngoding bareng bersama dhava");
+
+GamepadButton.Document(onclick);
+
+document("assalamualaikum");
+
+alert("walaikumsalam");
+
+const background = document.getElementById(String);
